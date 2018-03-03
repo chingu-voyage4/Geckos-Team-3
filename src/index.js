2a0e81a25b7909ccfa4c304423bb4f180a65a3e8
Adeen Shukla <adeens357@gmail.com>
2018-03-03 17:35:22 +0530

    Remove unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
-import firebaseConnfig from './utils/firebaseConfig';
 import './styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
